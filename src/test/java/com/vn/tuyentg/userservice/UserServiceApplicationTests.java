137fce2b45802ad8a7817cda0ab3e8e2ac2a2b4c
tuyentg <gawagaconan@gmail.com>
2022-07-02 10:28:11 +0700

    init project user service

---
+++ src/test/java/com/vn/tuyentg/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.tuyentg.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
