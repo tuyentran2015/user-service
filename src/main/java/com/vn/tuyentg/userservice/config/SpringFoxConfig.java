e2ac4c956c8daff40b1c38f4508b051c6290e1c2
tuyentg <gawagaconan@gmail.com>
2022-07-17 10:46:48 +0700

    updated: spring fox

--- src/main/java/com/vn/tuyentg/userservice/config/SpringFoxConfig.java
+++ src/main/java/com/vn/tuyentg/userservice/config/SpringFoxConfig.java
@@ -4,25 +4,12 @@ import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-
 @Configuration
 public class SpringFoxConfig {
-//	@Bean
-//	OpenAPI springShopOpenAPI() {
-//		return new OpenAPI()
-//				.info(new Info().title("SpringShop API").description("Spring shop sample application").version("v0.0.1")
-//						.license(new License().name("Apache 2.0").url("http://springdoc.org")))
-//				.externalDocs(new ExternalDocumentation().description("SpringShop Wiki Documentation")
-//						.url("https://springshop.wiki.github.org/docs"));
-//	}
 
 	@Bean
 	GroupedOpenApi publicApi() {
-		return GroupedOpenApi.builder().group("User service")
-				.packagesToScan("com.vn.tuyentg.userservice").pathsToMatch("/**").build();
+		return GroupedOpenApi.builder().group("User service").packagesToScan("com.vn.tuyentg.userservice")
+				.pathsToMatch("/**").build();
 	}
 }
